a4e43bad7cff36a91330c78e41a3e1dfc20c3f3d
sujin9335 <sujin93359335@gmail.com>
2023-11-16 16:03:21 +0900

    11-16_3

--- src/main/java/com/project/jr/study/mypage/StudyDetail.java
+++ src/main/java/com/project/jr/study/mypage/StudyDetail.java
@@ -19,8 +19,8 @@ public class StudyDetail extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-//		String id=req.getSession().getAttribute("id").toString();
-		String id="RvXc3eY";
+		String id=req.getSession().getAttribute("id").toString();
+		//String id="RvXc3eY";
 		
 		StudyDAO dao=new StudyDAO();
 		
