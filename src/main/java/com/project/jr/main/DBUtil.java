7d6ec724a1970b2b15fb8cca23ebd0bd3848717b
sujin9335 <sujin93359335@gmail.com>
2023-11-22 15:35:16 +0900

    11-22

--- src/main/java/com/project/jr/main/DBUtil.java
+++ src/main/java/com/project/jr/main/DBUtil.java
@@ -10,8 +10,8 @@ public class DBUtil {
 	public static Connection open() {
 		
 		
-		//String url = "jdbc:oracle:thin:@192.168.10.59:1521:xe";
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String url = "jdbc:oracle:thin:@192.168.10.59:1521:xe";
+		//String url = "jdbc:oracle:thin:@localhost:1521:xe";
 		String id = "jr";
 		String pw = "java1234";
 		
